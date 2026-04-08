d7eb95a3b3e1c7806e35533d2c32695e5043ebab
Gerry Mcbride <gerry.mcbride@trurisk.io>
2026-04-08 12:04:14 +0100

    Added new version update

--- src/wagtail_sitemap_seo/s3_helper.py
+++ src/wagtail_sitemap_seo/s3_helper.py
@@ -8,7 +8,6 @@ def save_xml(name: str, content: bytes) -> str:
     Save an XML file using Django's default storage.
 
     - In dev (SITEMAP_WRITE_S3): saves to MEDIA_ROOT
-    - In ECS/UAT (SITEMAP_WRITE_S3=True): saves to S3 bucket root (because your default storage is S3Boto3Storage)
 
     Returns the final storage path.
     """
